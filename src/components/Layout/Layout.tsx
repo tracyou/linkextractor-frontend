e7ecd0cf94b0378211e4bd11b313a932873f8748
pandemiek <mikarchristian@gmail.com>
2023-12-15 14:52:15 +0100

    fix: remove redundant /annotate route in Layout.tsx

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,35 +1,30 @@
 import React from 'react';
-import Navbar from "../Navbar/Navbar";  // Adjust the path based on your file structure
+import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
 import {Route, Routes} from "react-router-dom";
 import ImportXML from "../../views/ImportXML";
 import './Layout.css'
 import EditArticle from "../EditArticle/EditArticle";
-import TextAnnotator from "../TextAnnotator/TextAnnotator";
 
-const Layout = () => (
+const Layout = () => {
 
-
-    <div>
-        <div className="layout-container">
+    return (
+        <div>
+            <div className="layout-container">
 
             <Navbar></Navbar>
             <switch>
                 <div className="content-area">
-
                     <Routes>
                         <Route path="/import-xml" element={<ImportXML/>}/>
                         <Route path="/navbar" element={<Navbar/>}/>
                         <Route path="/editarticle" element={<EditArticle/>}/>
-                        <Route path="/annotate" element={<TextAnnotator/>}/>
                     </Routes>
                 </div>
-
-            </switch>
-            <Footer></Footer>
+                </switch>
+                <Footer></Footer>
+            </div>
         </div>
-
-    </div>
-);
-
+    )
+};
 export default Layout;
