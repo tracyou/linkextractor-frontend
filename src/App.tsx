6a8a512e996fa4a9ed149afa661f44e908eba450
pandemiek <mikarchristian@gmail.com>
2023-12-15 14:47:34 +0100

    feat: add recoil wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import './App.css';
 import {RouterProvider} from "react-router-dom";
 import router from "./react-router";
 import CssBaseline from '@mui/material/CssBaseline';
+import {RecoilRoot} from "recoil";
 
 function App() {
     return (
-        <>
+        <RecoilRoot>
             <CssBaseline enableColorScheme={true}/>
             <RouterProvider router={router}/>
-        </>
+        </RecoilRoot>
     );
 }
 
