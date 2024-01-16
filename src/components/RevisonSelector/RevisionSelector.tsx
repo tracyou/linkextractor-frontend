8bc924fecdd413b72bc4c4398371bfd2b0b3472d
Roy Wiersma <36405434+RoyWiersma@users.noreply.github.com>
2024-01-16 11:39:21 +0100

    Added Revision Stepper UI

--- src/components/RevisonSelector/RevisionSelector.tsx
+++ src/components/RevisonSelector/RevisionSelector.tsx
@@ -1,75 +1,33 @@
 import EditArticle from "../EditArticle/EditArticle";
-import React, {useEffect, useState} from "react";
-import {MenuItem, Tab, Tabs,} from "@mui/material";
+import React from "react";
+import {createTheme, Step, StepButton, Stepper} from "@mui/material";
 import Box from "@mui/material/Box";
 import {useParams} from "react-router-dom";
-import {Title} from "../../stylesheets/Fonts";
-import {useQuery} from "@apollo/client";
-import {ArticleRevisionFragment, GetLawByIdDocument, GetLawByIdQuery, LawFragment} from "../../graphql/api-schema";
 
 const RevisionSelector = () => {
     // Get id from the router
     const {id} = useParams();
-    const [lawId, setLawId] = useState<string | undefined>();
-    const [revisionData, setRevisionData] = useState<number | undefined>();
-    useEffect(() => {
-        setLawId(id);
-    }, [id]);
-    // Query to get data
-    const {
-        data: queryResult,
-        loading: revisionLoading,
-        error: revisionError
-    } = useQuery<GetLawByIdQuery>(GetLawByIdDocument, {
-        variables: {
-            id: lawId
-        }
-    });
 
-    useEffect(() => {
-        if (revisionLoading) {
-            return
-        }
-
-        if (queryResult) {
-            setRevisionData(queryResult.law.revision);
-            console.log(queryResult.law.revision)
-        }
-    }, [revisionLoading]);
-
-    // Setting the data in the
-
-    const [value, setValue] = React.useState(0);
-
-    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-        setValue(newValue);
+    // Stepper logic
+    const steps = ['Versie: 1, Datum: 16-1-2024, Tijd: 11:10', 'Versie: 2, Datum: 16-1-2024, Tijd: 11:11', 'Versie: 3, Datum: 16-1-2024, Tijd: 11:15'];
+    const [activeStep, setActiveStep] = React.useState(0);
+    const handleStep = (step: number) => () => {
+        setActiveStep(step);
     };
 
     return (
-        <Box>
-              <>
-                <h1 style={Title}>Versiebeheer</h1>
-                <Tabs
-                  value={value}
-                  onChange={handleChange}
-                  variant="scrollable"
-                  scrollButtons
-                  allowScrollButtonsMobile
-                  aria-label="scrollable force tabs example"
-                  sx={{
-                      '& .MuiTabs-scrollButtons.Mui-disabled': {
-                          opacity: 0.3
-                      }
-                  }}
-                >
-                    {
-
-                    }
-                </Tabs>
-              </>
-            {/*Passing revision id to the editarticle*/}
-            <EditArticle/>
-        </Box>
+        <Box paddingTop={'1.5rem'}>
+            {steps.length > 1 &&
+              <Stepper nonLinear activeStep={activeStep}>
+                  {steps.map((label, index) => (
+                      <Step key={label}>
+                          <StepButton color="inherit" onClick={handleStep(index)}>
+                              {label}
+                          </StepButton>
+                      </Step>
+                  ))}
+              </Stepper>}
+            <EditArticle/></Box>
     );
 };
 export default RevisionSelector;
