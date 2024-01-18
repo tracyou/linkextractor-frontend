fe267141207584e319f3e387318f2aeefcb0cf2c
Roy Wiersma <36405434+RoyWiersma@users.noreply.github.com>
2024-01-18 15:18:44 +0100

    Added new query

--- src/components/RevisonSelector/RevisionSelector.tsx
+++ src/components/RevisonSelector/RevisionSelector.tsx
@@ -1,16 +1,44 @@
 import EditArticle from "../EditArticle/EditArticle";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Tab, Tabs,} from "@mui/material";
 import Box from "@mui/material/Box";
 import {useParams} from "react-router-dom";
 import {Title} from "../../stylesheets/Fonts";
+import {useQuery} from "@apollo/client";
+import {GetLawRevisionsDocument, GetLawRevisionsQuery,} from "../../graphql/api-schema";
 
 const RevisionSelector = () => {
     // Get id from the router
     const {id} = useParams();
+    const [lawId, setLawId] = useState<string | undefined>();
+    const [revisionData, setRevisionData] = useState<number | undefined>();
+    useEffect(() => {
+        setLawId(id);
+    }, [id]);
+    // Query to get data
+    const {
+        data: queryResult,
+        loading: revisionLoading,
+        error: revisionError
+    } = useQuery<GetLawRevisionsQuery>(GetLawRevisionsDocument, {
+        variables: {
+            id: lawId
+        }
+    });
 
-    // Stepper logic
-    const steps = ['Datum: 16-1-2024, Tijd: 11:10', 'Datum: 16-1-2024, Tijd: 11:11', 'Datum: 16-1-2024, Tijd: 11:11', 'Datum: 16-1-2024, Tijd: 11:11', 'Datum: 16-1-2024, Tijd: 11:11', 'Datum: 16-1-2024, Tijd: 11:11', 'Datum: 16-1-2024, Tijd: 11:11', 'Datum: 16-1-2024, Tijd: 11:11', 'Datum: 16-1-2024, Tijd: 11:11', 'Datum: 16-1-2024, Tijd: 11:11', 'Datum: 16-1-2024, Tijd: 11:11', 'Datum: 16-1-2024, Tijd: 11:11', 'Datum: 16-1-2024, Tijd: 11:11', 'Datum: 16-1-2024, Tijd: 11:11', 'Datum: 16-1-2024, Tijd: 11:11', 'Datum: 16-1-2024, Tijd: 11:11', 'Datum: 16-1-2024, Tijd: 11:11', 'Datum: 16-1-2024, Tijd: 11:11', 'Datum: 16-1-2024, Tijd: 11:11', 'Datum: 16-1-2024, Tijd: 11:11', 'Datum: 16-1-2024, Tijd: 11:11', 'Datum: 16-1-2024, Tijd: 11:11', 'Datum: 16-1-2024, Tijd: 11:11', 'Datum: 16-1-2024, Tijd: 11:11', 'Datum: 16-1-2024, Tijd: 11:11', 'Datum: 16-1-2024, Tijd: 11:11', 'Datum: 16-1-2024, Tijd: 11:11', 'Datum: 16-1-2024, Tijd: 11:11', 'Datum: 16-1-2024, Tijd: 11:11', 'Datum: 16-1-2024, Tijd: 11:11', 'Datum: 16-1-2024, Tijd: 11:11', 'Datum: 16-1-2024, Tijd: 11:11', 'Datum: 16-1-2024, Tijd: 11:11'];
+    useEffect(() => {
+        if (revisionLoading) {
+            return
+        }
+
+        if (queryResult) {
+            setRevisionData(queryResult);
+            console.log(queryResult)
+        }
+    }, [revisionLoading]);
+
+    // Setting the data in the
+
     const [value, setValue] = React.useState(0);
 
     const handleChange = (event: React.SyntheticEvent, newValue: number) => {
@@ -19,28 +47,29 @@ const RevisionSelector = () => {
 
     return (
         <Box>
-            {steps.length > 1 &&
-              <>
+            <>
                 <h1 style={Title}>Versiebeheer</h1>
                 <Tabs
-                  value={value}
-                  onChange={handleChange}
-                  variant="scrollable"
-                  scrollButtons
-                  allowScrollButtonsMobile
-                  aria-label="scrollable force tabs example"
-                  sx={{
-                      '& .MuiTabs-scrollButtons.Mui-disabled': {
-                          opacity: 0.3
-                      }
-                  }}
+                    value={value}
+                    onChange={handleChange}
+                    variant="scrollable"
+                    scrollButtons
+                    allowScrollButtonsMobile
+                    aria-label="scrollable force tabs example"
+                    sx={{
+                        '& .MuiTabs-scrollButtons.Mui-disabled': {
+                            opacity: 0.3
+                        }
+                    }}
                 >
-                    {steps.map((label) => (
-                        <Tab key={label} label={label}/>
-                    ))}
+                    {
+                        Object.entries(revisionData).map(([,value]) => {
+                            return (<Tab key={} label={value}/>)
+                    }
+                    )
+                    }
                 </Tabs>
-              </>
-            }
+            </>
             {/*Passing revision id to the editarticle*/}
             <EditArticle/>
         </Box>
